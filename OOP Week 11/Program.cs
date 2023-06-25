b574f4803a5b1327151bf4e258af88fc810826ea
shahzaibsalem <120157156+shahzaibsalem@users.noreply.github.com>
2023-06-25 21:41:54 +0500

    Add files via upload

---
+++ OOP Week 11/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EZInput;
+using PacMan.GameGL;
+
+namespace PacMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameGrid grid = new GameGrid("maze.txt", 23, 71);
+            GameCell start = new GameCell(12, 22, grid);
+            GamePacManPlayer pacman = new GamePacManPlayer('p', start);
+            printMaze(grid);
+            printGameObject(pacman);
+
+
+            bool gameRunning = true;
+            while (gameRunning)
+            {
+                Thread.Sleep(90);
+                if (Keyboard.IsKeyPressed(Key.UpArrow))
+                {
+                    moveGameObject(pacman, GameDirection.Up);
+                }
+
+                if (Keyboard.IsKeyPressed(Key.DownArrow))
+                {
+                    moveGameObject(pacman, GameDirection.Down);
+                }
+
+                if (Keyboard.IsKeyPressed(Key.RightArrow))
+                {
+                    moveGameObject(pacman, GameDirection.Right);
+                }
+
+                if (Keyboard.IsKeyPressed(Key.LeftArrow))
+                {
+                    moveGameObject(pacman, GameDirection.Left);
+                }
+            }
+        }
+        static void clearGameCellContent(GameCell gameCell, GameObject newGameObject)
+        {
+            gameCell.CurrentGameObject = newGameObject;
+            Console.SetCursorPosition(gameCell.Y, gameCell.X);
+            Console.Write(newGameObject.DisplayCharacter);
+
+        }
+        static void printGameObject(GameObject gameObject)
+        {
+            Console.SetCursorPosition(gameObject.CurrentCell.Y, gameObject.CurrentCell.X);
+            Console.Write(gameObject.DisplayCharacter);
+
+        }
+
+        static void moveGameObject(GameObject gameObject, GameDirection direction)
+        {
+            GameCell nextCell = gameObject.CurrentCell.nextCell(direction);
+            if (nextCell != null)
+            {
+                GameObject newGO = new GameObject(GameObjectType.NONE, ' ');
+                GameCell currentCell = gameObject.CurrentCell;
+                clearGameCellContent(currentCell, newGO);
+                gameObject.CurrentCell = nextCell;
+                printGameObject(gameObject);
+            }
+        }
+
+        static void printMaze(GameGrid grid)
+        {
+            for (int x = 0; x < grid.Rows; x++)
+            {
+                int abc = 0;
+                for (int y = 0; y < grid.Cols; y++)
+                {
+                    GameCell cell = grid.getCell(x, y);
+                    printCell(cell);
+                }
+
+            }
+        }
+
+        static void printCell(GameCell cell)
+        {
+            Console.SetCursorPosition(cell.Y, cell.X);
+            Console.Write(cell.CurrentGameObject.DisplayCharacter);
+        }
+
+    }
+}
